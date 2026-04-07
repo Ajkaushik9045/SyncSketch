7c2a3df23c474e5ec74b73cc46fbac8a5c20691b
Ajkaushik9045 <anujkaushik2609@gmail.com>
2026-04-07 23:57:36 +0530

    Testing Theme and Colour INtegration

--- syncsketch/lib/app/app.dart
+++ syncsketch/lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:syncsketch/core/theme/app_theme.dart';
 import 'package:syncsketch/core/theme/cubit/theme_cubit.dart';
+import 'package:syncsketch/features/home/home.dart';
 
 class App  extends StatelessWidget{
   const App({super.key});
@@ -11,11 +12,10 @@ class App  extends StatelessWidget{
     return BlocBuilder<ThemeCubit, ThemeMode>(builder: (context, mode){
       return MaterialApp(
         debugShowCheckedModeBanner: false,
-
         theme: AppTheme.light,
         darkTheme: AppTheme.dark,
         themeMode: mode,
-        home: ,
+        home: const Home(),
       );
     });
   }
